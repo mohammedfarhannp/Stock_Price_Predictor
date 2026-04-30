5568ccaad944476920e85fe138cc0f16570e263a
mohammedfarhannp <m.farhan.np@gmail.com>
2026-04-30 17:08:35 +0530

    File Name updated!

--- git_commit.py
+++ git_commit.py
@@ -1,7 +1,7 @@
 import os
 
 Commands = [
-    "git add data\\raw\\ROLEXRINGS_NS_01-01-2025_to_CURRENT.csv",
+    "git add data\\raw\\SUZLON_NS_01-01-2025_to_CURRENT.csv",
     "git commit -m \"New Data Added!\" -m \"Automated Commit!\"",
     "git add data\\processed\\processed_data.csv",
     "git commit -m \"New Processed Data!\" -m \"Automated Commit!\"",
